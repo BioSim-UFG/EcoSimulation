3e7f0cf438b63d6dcef0d146b3f76814aafe8915
João Gabriel <jgabsfernandes@gmail.com>
2018-08-21 13:45:04 -0300

    Mini interface para execução do programa - em progresso

--- code_in_C-C++/PaleoData.cpp
+++ code_in_C-C++/PaleoData.cpp
@@ -3,12 +3,17 @@
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string>
 
 using namespace std;
 
 int main(){
 
-    ifstream presentClimFile, paleoClimFile;
+    string presentClimFile, paleoClimFile, r;
+    string outSuffix;
+    double b, e, s; 
+
     string input_str(100, '\0');
 
 
@@ -29,14 +34,14 @@ int main(){
     printf("7) Annual NPP\n\n");
     printf("Type ENTER or \"Y\" if yes, or type the correct name of the paleoclimate file\n");
 
-    scanf("%[^\n]s", &input_str[0]);
+    scanf(" %[^\n]s", &input_str[0]);
 
     if(input_str[0] == 'Y' || input_str.find("yes") !=-1  || input_str.empty() ){
-        presentClimFile.open("Coords and Clim.txt");
+        presentClimFile = "Coords and Clim.txt";
     }else{
+        presentClimFile = input_str;
         printf("Enter the number of cells(lines) in the grid: ");
         scanf("%d", &numero_de_linhas);
-        presentClimFile.open(input_str);
     }
 
     system("clear");    //for unix
@@ -45,15 +50,68 @@ int main(){
     printf("Is the paleoclimate file ' 'xPLASIM.All3DMats.Single.Zip.Stream' '?\n");
     printf("Type ENTER or \"Y\" if yes, or the correct name of the paleoclimate file\n");
 
-    scanf("%[^\n]s", &input_str[0]);
+    scanf(" %[^\n]s", &input_str[0]);
 
     if (input_str[0] == 'Y' || input_str.find("yes") != -1 || input_str.empty()) {
-        paleoClimFile.open("xPLASIM.All3DMats.Single.Zip.Stream");
+        paleoClimFile = "xPLASIM.All3DMats.Single.Zip.Stream";
     }
     else {
-        scanf("%d", &numero_de_linhas);
-        paleoClimFile.open(input_str);
+        paleoClimFile = input_str;
     }
+
+    system("clear");    //for unix
+    //system("CLS");      //for DOS ( windows);
+
+    printf("Would you like to use the suffix '!OutPaleoClim - Var.txt*'?\n\n");
+    printf("Type ENTER or \"Y\" if yes, or type the suffix for the output files\n");
+    printf("If you would like to customize the suffix, enter only a single word (e.g. ''ClimDat'')\n");
+
+    scanf(" %[^\n]s", &input_str[0]);
+
+    if( input_str[0] == 'Y' || input_str.find("yes") != -1 || input_str.empty() ){
+        outSuffix = "!OutPaleoClim";
+    }else{
+        outSuffix = input_str;
+    }
+
+    system("clear");    //for unix
+    //system("CLS");      //for DOS ( windows);
+
+    printf("Would you like to start 5M years ago?\n\n");
+    printf("Type ENTER or \"Y\" if yes, or enter the starting time in thousand of years ago (5000 = 5Mya)\n");
+
+    scanf(" %[^\n]s", &input_str[0]);
+
+    if( input_str[0] == 'Y' || input_str.find("yes") != -1 || input_str.empty() ){
+        b = 5000.0;
+    }else{
+        b = stod(input_str, NULL);
+    }
+
+    printf("Would you like to end in the present time?\n\n");
+    printf("Type ENTER or \"Y\" if yes, or enter the ending time in thousand of years ago (1000 = 1Mya)\n");
+
+    scanf(" %[^\n]s", &input_str[0]);
+
+    if( input_str[0] == 'Y' || input_str.find("yes") != -1 || input_str.empty() ){
+        e = 0;
+    }else{
+        e = stod(input_str);
+    }
+
+    printf("Would you like to use 500 years as time step?\n\n");
+    printf("Type ENTER or \"Y\" if yes, or enter the length of time step in thousand of years ago (0.5 = 500y)\n");
+
+    scanf(" %[^\n]s", &input_str[0]);
+
+    if( input_str[0] == 'Y' || input_str.find("yes") != -1 || input_str.empty() ){
+        s = 0.5;
+    }else{
+        s = stod(input_str);
+    }
+
+
+
 
 
 
