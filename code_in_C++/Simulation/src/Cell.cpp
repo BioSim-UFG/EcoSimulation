2337896e5652498e37e48851761807ad62aed88e
João Gabriel <jgabsfernandes@gmail.com>
2018-08-29 11:22:09 -0300

    Classe da célula

---
+++ code_in_C++/Simulation/src/Cell.cpp
@@ -0,0 +1,18 @@
+
+
+namespace SimEco
+{
+
+    Cell::Cell(){ id = totalCells++; }
+    Cell::~Cell(){ --totalCells; }
+    // A continuous value of suitability, é a função que ja fizemos em GPU
+    Cell::getLocalSuitabilities(){
+
+
+    }
+    
+
+
+    
+
+} // SimEco
--- code_in_C++/Simulation/src/Cell.h
+++ code_in_C++/Simulation/src/Cell.h
@@ -1,7 +1,7 @@
 #include <vector>
 #include <array>
 
-#define ENV_NUM_VARS 4
+#define ENV_NUM_VARS 2
 #define NUM_TOTAL_CELLS 20
 
 using namespace std;
@@ -12,29 +12,47 @@ namespace SimEco
 
     typedef struct{
         float maximum;          // Annual maxima per cell, in a given time, for a given variable
-        float center;            // Midpoint between max and min
+        //float center;            // Midpoint between max and min
         float minimum;          // Annual minima per cell, in a given time, for a given variable
     }EnvValue, NicheValue;
 
     typedef struct{
         //usar array se for tamanho fixo, e vector se for dinamico
-        array<EnvValue, ENV_NUM_VARS> envValues;    // Temperature and precipitation
-        float *NPP;
+        array<EnvValue, ENV_NUM_VARS> envValues;    // Temperature and precipitation -> max and min for both
+        float *NPP; //melhor ser um float, ao inves de float*, não? ( float = 32 bis e ponteiro = 64bits)
     }Climate;
 
 
 
     class Cell{
+        //variavel da classe, que indica o total de celulas criadas
+        static int totalCells=0;
+
         public:
         int id;
         
         float area;
-        Climate climate;        // Environmental variables for each cell
+
+        /* coloquei um vetor da classe ( nao objeto) para melhor acesso e perfomance,
+         e também por ser assim que os dados são acessados e tratados na GPU
+         */
+        //Climate climate;        // Environmental variables for each cell
+        static Climate* cell_climates;  //aqui vai ser um vetor com todos os dados das celulas
 
         vector<float> geoConn;              // Degree of connectivity (1 / distance) between this cell and all other cells, based on geographic distance
         vector<float> geoConn;              // Degree of connectivity (1 / distance) between this cell and all other cells, based on topographic heterogeneity
         vector<float> geoConn;              // Degree of connectivity (1 / distance) between this cell and all other cells, based on river network
 
+        Cell();
+        ~Cell();
+
+        /* melhor nao ter uma função que calcula a suitability de apenas uma celula ( falta de paralelismo)
+        // A continuous value of suitability
+        //Function GetLocalSuitability(LocalEnv: TEnvValues; Niche: TNicheValues): Single;
+        */
+
+        // A continuous value of suitability, é a função que ja fizemos em GPU
+        static void getLocalSuitabilities();
     };
 
 
