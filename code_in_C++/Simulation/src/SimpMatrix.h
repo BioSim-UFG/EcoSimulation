8313cfcc61391b071f1d4bccb9eef800deacbd9c
Felipe Gemmal <lipegemmal@hotmail.com>
2018-09-13 08:23:48 -0300

    Teste de estrutura

---
+++ code_in_C++/Simulation/src/SimpMatrix.h
@@ -0,0 +1,19 @@
+#include <vector>
+#include <utility>
+
+namespace SimEco{
+
+    class simpMatrix{
+
+        std::vector<unsigned int> num;
+        std::vector< std::pair<unsigned int, unsigned int> > pos;
+
+        
+
+
+
+    };
+
+
+
+}
